e63cc6d0caa120a48dd271005221f5ece40c01b2
Thip <thip.software@gmail.com>
2021-05-30 21:14:01 +0300

    feat: fixed config

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,12 +5,22 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
+declare var process : {
+  env: {
+    PORT: number
+    NODE_ENV: string,
+    MONGO_CONNECTION_STRING: string,
+    JWT_SECRET_KEY: string,
+    AUTH_MODE: string
+  }
+}
+
 const config = {
-  PORT: process.env['PORT'],
-  NODE_ENV: process.env['NODE_ENV'],
-  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
-  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
-  AUTH_MODE: process.env['AUTH_MODE'] === 'true'
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  AUTH_MODE: process.env.AUTH_MODE === 'true'
 };
 
 export { config };
