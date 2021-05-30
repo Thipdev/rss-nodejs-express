0a9bccfc42dc1474b9d46a1eeee9fe6681e03f8e
Thip <thip.software@gmail.com>
2021-05-30 20:10:36 +0300

    feat: task router

--- src/resources/tasks/task.router.ts
+++ src/resources/tasks/task.router.ts
@@ -5,17 +5,17 @@ const router = express.Router({ mergeParams: true });
 const service = new TaskService();
 
 router.route('/')
-    .get(async (req, res) => {
+    .get(async (req: any, res) => {
         const tasks = await service.getAll(req.params.boardId);
         res.json(tasks);
     })
-    .post(async (req, res) => {
+    .post(async (req: any, res) => {
         const task = await service.addTask(req.params.boardId, req.body);
         res.status(201).json(task);
     });
 
 router.route('/:id')
-    .get(async (req, res) => {
+    .get(async (req: any, res) => {
         const task = await service.getById(req.params.boardId, req.params.id);
         if(!task) {
             res.status(404).json({ error: 'Task was not found.' });
@@ -24,7 +24,7 @@ router.route('/:id')
       
         res.json(task);
     })
-    .put(async (req, res) => {
+    .put(async (req: any, res) => {
         const task = await service.updateTask(req.params.boardId, req.params.id, req.body);
         if(!task) {
             res.status(400).json({ error: 'Task was not found.' });
@@ -33,7 +33,7 @@ router.route('/:id')
 
         res.json(task);
     })
-    .delete(async (req, res) => {
+    .delete(async (req: any, res) => {
         const result = await service.deleteTask(req.params.boardId, req.params.id);
         if(!result) {
             res.status(404).json({ error: 'Task was not found.' });
