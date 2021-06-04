03ef3610d0c862b9df93f502ef41923904c1a95b
Thip <thip@rambler.ru>
2021-06-04 16:58:38 +0300

    feat: fix error logger

--- src/middlewares/error.handling.ts
+++ src/middlewares/error.handling.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { stderr } from 'process';
 
 const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-    stderr.write('err.message');
+    stderr.write(`${err.message}`);
     res.status(500).send({ error: err.message });
 };
 
